094c1ac567a5f17670058f088b48308aaf1c9c7f
btocoolman <bvh999@gmail.com>
2017-02-03 20:51:24 -0500

    finished #2

--- 496HW2.rkt
+++ 496HW2.rkt
@@ -15,65 +15,70 @@
     (leaf-t (datum) 1)
     (node-t (s left right) (+ (+ (dTree-size left) (dTree-size right)) 1))))
 
-;; dTree 
+;; dTree -> num
 (define (dTree-height tree)
   (cases dTree tree
     (leaf-t (datum) 0)
-    (node-t (s left right) (max (+ (dTree-size left) 1) (+ (dTree-size right) 1)))))
+    (node-t (s left right) (max (+ (dTree-height left) 1) (+ (dTree-height right) 1)))))
 
+;; dTree -> [[num]]
 (define (dTree-paths tree)
   (cases dTree tree
     (leaf-t (datum) '(()))
     (node-t (s l r) (append (map (lambda (list) (cons 0 list)) (dTree-paths l))
                             (map (lambda (list) (cons 1 list)) (dTree-paths r))))))
 
+;; dTree -> bool
 (define (dTree-perfect? tree)
-  (cases dTree tree
-    (leaf-t (datum) 0)
-    (node-t (s l r) (equal? (+ (dTree-height l) 1) (+ (dTree-height r) 1)))))
+  (define paths (dTree-paths tree))
+  (if (empty? paths)
+      #t
+      (dTree-Perfect? (cdr paths) (length (car paths)))))
 
+;; {[[num]], num} -> bool
+(define (dTree-Perfect? paths len)
+  (if (empty? paths)
+      #t
+      (if (equal? len (length (car paths)))
+          (dTree-Perfect? (cdr paths) len)
+          #f)))
+
+;; { (symbol -> symbol), (num -> num), dTree} -> dTree
 (define (dTree-map f g t)
   (cases dTree t
     (leaf-t (datum) (leaf-t (g datum)))
     (node-t (s l r) (node-t (f s) (dTree-map f g l) (dTree-map f g r)))))
 
+;; [symbol] -> dTree
 (define (list->tree list)
   (if (empty? list)
       (leaf-t 0)
       (node-t (car list) (list->tree (cdr list)) (list->tree (cdr list)))))
 
-;;(define (replaceLeafAt2 f t)
-;;  (if (empty? f)
-;;      t
-;;      (replaceLeafAt (cdr f) (replaceLeafHelper (car f) t))))
-
-;;(define (replaceLeafHelper2 pair t)
-;;  (cases dTree t
-;;    (leaf-t (n) (leaf-t (cdr pair))
-;;    (node-t (n l r)
-;;            (if (zero? (caar pair))
-;;                (node-t n (replaceLeafHelper (cons (cdar pair) (cdr pair)) l) r)
-;;                (node-t n l (replaceLeafHelper (cons (cdar pair) (cdr pair)) r))
-;;                )))))
-
+;; { ([num], num), dTree } -> dTree 
 (define (replaceLeafAt f t)
-  (if (null? (cdr f))
+  (if (empty? f)
       t
-      (replaceLeafAt (cdr f) (replaceLeafHelper (car (cadr f)) (cdr (cadr f)) t))))
+      (replaceLeafAt (cdr f) (replaceLeafHelper (car f) t))))
 
-(define (replaceLeafHelper path value t)
+;; { ([num], num), dTree } -> dTree
+(define (replaceLeafHelper pair t)
   (cases dTree t
-    (leaf-t (n) (leaf-t value))
+    (leaf-t (n) (leaf-t (cdr pair)))
     (node-t (n l r)
-            (if (zero? (car path))
-                (node-t n (replaceLeafHelper (cdr path) value l) r)
-                (node-t n l (replaceLeafHelper (cdr path) value r))
-                ))))
-      
+            (if (zero? (caar pair))
+                (node-t n (replaceLeafHelper (cons (cdar pair) (cdr pair)) l) r)
+                (node-t n l (replaceLeafHelper (cons (cdar pair) (cdr pair)) r))))))
 
+;; ( [symbol], [([num], num)] ) -> dTree
+(define (bf->dTree pairE)
+  (replaceLeafAt (cdr pairE) (list->tree (car pairE))))
+
+;; symbol -> symbol
 (define symbol-upcase
   (compose string->symbol (compose string-upcase symbol->string)))
 
+;; num -> num
 (define (succ n)
   (+ n 1))
 
